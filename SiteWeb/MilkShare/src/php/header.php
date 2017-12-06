ea2085e713856b849378e50bb7aea88fd78ff3d4
Zino <valzinobe@etml.educanet2.ch>
2017-12-06 15:00:59 +0100

    Ajout du site

---
+++ SiteWeb/MilkShare/src/php/header.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ *Author: Benjamin Valzino
+ *Date: 29.11.2017
+ *Location: ETML
+ *Description:
+ */
+?>
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="Accueil.php"><img src="../../resources/images/logo.png" width="20%" height="20%" class="mr-2"><b>MilkShare©</b></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar2SupportedContent" aria-controls="navbar2SupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-end" id="navbar2SupportedContent">
+        <ul class="navbar-nav text-white">
+          <li class="nav-item">
+            <a class="nav-link text-white" href="Accueil.php"><i class="fa d-inline fa-lg fa-home px-2"></i>Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="CarteCommande.php"><i class="fa d-inline fa-lg fa-map-o px-2"></i>Commandes</a>
+          </li>
+        </ul>
+        <a class="btn navbar-btn ml-2 text-white btn-secondary"><i class="fa d-inline fa-lg fa-user-circle-o"></i>S'inscrire</a>
+      </div>
+    </div>
+  </nav>
+</header>
